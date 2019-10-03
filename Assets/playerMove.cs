828991602f2262d3cd6a3601919a117a5d69c26a
LHussell <46260172+LHussell@users.noreply.github.com>
2019-10-03 18:37:21 +0100

    First upload of small game project.

---
+++ Assets/playerMove.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMove : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+    public int speed = 3;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            rb.AddForce(new Vector2(0, 1) * speed);
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            rb.AddForce(new Vector2(0, -1) * speed);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            rb.AddForce(new Vector2(1, 0) * speed);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            rb.AddForce(new Vector2(-1, 0) * speed);
+        }
+
+    }
+}
